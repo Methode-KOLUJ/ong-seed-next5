b4a44537331f73855d785880116b6de070b436c1
Monsieur KOLUJ <misterfrpkoluj@gmail.com>
2025-07-26 09:58:40 +0200

    Update GoogleAd.tsx

--- components/GoogleAd.tsx
+++ components/GoogleAd.tsx
@@ -32,7 +32,7 @@ export default function GoogleAd({
       {children}
       <ins
         className="adsbygoogle"
-        style={{ display: 'hidden' }}
+        style={{ display: 'block' }}
         data-ad-client="ca-pub-8295063989085416"
         data-ad-slot={adSlot}
         data-ad-format={format}
